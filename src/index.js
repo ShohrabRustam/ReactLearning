fb6fa107b88ff5fc33167a0efa4c9ee57a6a4911
Shohrab Rustam <mohdrustam001@gmail.com>
2022-06-27 12:54:05 +0530

    first program in react

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-// import './index.css';
-var React = new require('react');
-var ReactDOM = new require('react-dom');
-ReactDOM.render(<h1>Hello World</h1>,document.getElementById('root')); 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//  <h1>Hello World !!!</h1>
-// );
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+// var React = new require('react');
+// var ReactDOM = new require('react-dom');
+// ReactDOM.render(<h1>Hello World</h1>,document.getElementById('root')); 
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+ <h1>Hello World !!!</h1>
+);
