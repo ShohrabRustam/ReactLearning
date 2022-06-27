493212566fa24c28cb5ab208195e79fb601af912
Shohrab Rustam <mohdrustam001@gmail.com>
2022-06-27 17:42:39 +0530

    some componet undertstanding in react

---
+++ src/Components/Heading.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Heading(){
+    return <h1>My name is<span style={{color:'Blue'}}> Shohrab Rustam</span></h1>;
+}
+
+export default Heading;
--- src/index.js
+++ src/index.js
@@ -1,40 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import Heading from './Components/Heading';
+import Para from './Components/Para';
 // var React = new require('react');
 // var ReactDOM = new require('react-dom');
 // ReactDOM.render(<h1>Hello World</h1>,document.getElementById('root')); 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const time= new Date(/* 2020, 3,2,8 */);
-const cssStyle={
-    color:'green'
+const time = new Date(/* 2020, 3,2,8 */);
+var cssStyle = {
+    color: 'green'
 }
 const h = time.getHours();
 // console.log(h);
-var color = ''; 
-var greeting = '' 
-if(h>=5&& h<=11){
-    greeting= "Good Morning"
+var color = '';
+var greeting = ''
+if (h >= 5 && h <= 11) {
+    greeting = "Good Morning"
     cssStyle.color = 'green'
 }
-else if(h>=12 &&    h<=15)  {
-    greeting= "Good Afternoon"
+else if (h >= 12 && h <= 15) {
+    greeting = "Good Afternoon"
     cssStyle.color = 'blue'
 
-}else if(h>=16 && h<=18){
-    greeting= "Good Evening"
+} else if (h >= 16 && h <= 18) {
+    greeting = "Good Evening"
     cssStyle.color = 'red'
 
-}else{
-    greeting= "Good Night"
+} else {
+    greeting = "Good Night"
     cssStyle.color = 'yellow'
 
 }
 
 root.render(
     <div>
-    <h1>Hello Sir, <span style={cssStyle}> {greeting} </span></h1>
+        <Heading></Heading>,
+        <Para></Para>,
+        <h1>Hello Sir, <span style={cssStyle}> {greeting} </span></h1>
     </div>
+
 );
 
 
