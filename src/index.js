f6501596fc395f777e660bd93465bec6dd362458
Shohrab Rustam <mohdrustam001@gmail.com>
2022-06-28 13:38:33 +0530

    data using the indexa afyer storing inside the array of onbject

--- src/index.js
+++ src/index.js
@@ -27,28 +27,63 @@ import Card from './Components/Card';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <div style={{display: 'flex', }} >
-   <Card imgsrc= "https://wallpaperaccess.com/full/1510470.jpg" 
-   card_category= "A Netflix Original Series" 
-   card_title= "DARK"
-   href="https://www.netflix.com/in/title/80100172"
-   />
-   <Card imgsrc= "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4pGDx_jOa5-K9Jlc1WaVIg53AFspQ9rx6Sg&usqp=CAU" 
-   card_category= "A Netflix Original Series" 
-   card_title= "Stranger Things"
-   href="https://www.netflix.com/in/title/80057281" />
+const data = [{
+    imgsrc: "https://wallpaperaccess.com/full/1510470.jpg",
+    card_category: 'A Netflix Original Series',
+    card_title: 'DARK',
+    link: 'https://www.netflix.com/in/title/80100172'
+},
+{
+    imgsrc: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4pGDx_jOa5-K9Jlc1WaVIg53AFspQ9rx6Sg&usqp=CAU",
+    card_category: 'A Netflix Original Series',
+    card_title: 'Stranger Things',
+    link: 'https://www.netflix.com/in/title/80057281'
+},
+{
+    imgsrc: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTi-8UGYwmCK3hg0wwFzdwXBkqEwdkJSwFq-Q&usqp=CAU",
+    card_category: 'A Netflix Original Series',
+    card_title: 'Peaky Blinders',
+    link: 'https://www.netflix.com/in/title/80002479'
+},
+]
 
-<Card imgsrc= "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTi-8UGYwmCK3hg0wwFzdwXBkqEwdkJSwFq-Q&usqp=CAU" 
-   card_category= "A Netflix Original Series" 
-   card_title= "Peaky Blinders "
-   href="https://www.netflix.com/in/title/80002479" />
-{/* 
-   <Card imgsrc= "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRC1PZp2noE85jIlg4X0msgtvC0-zQqNLpgQ&usqp=CAU" 
-   card_category= "A Netflix Original Series" 
-   card_title= "Lucifer"
-   href="https://www.netflix.com/in/title/80057918" /> */}
-   </div>
+// for (let i = 0; i < data.length; i++) {
+//     console.log(data);
+// }
+
+root.render(
+    <>
+        <h1 className="Heading_title">List of Top 5 Netflix Series</h1>
+
+
+        <div style={{ display: 'flex', }} >
+            <Card imgsrc={data[0].imgsrc}
+                card_category={data[0].card_category}
+                card_title={data[0].card_title}
+                href={data[0].link}
+            />
+            <Card imgsrc={data[1].imgsrc}
+                card_category={data[1].card_category}
+                card_title={data[1].card_title}
+                href={data[1].link}
+            />
+            <Card imgsrc={data[2].imgsrc}
+            card_category={data[2].card_category}
+            card_title={data[2].card_title}
+            href={data[2].link}
+        />
+        {/* 
+            <Card imgsrc="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4pGDx_jOa5-K9Jlc1WaVIg53AFspQ9rx6Sg&usqp=CAU"
+                card_category="A Netflix Original Series"
+                card_title="Stranger Things"
+                href="https://www.netflix.com/in/title/80057281" />
+
+            <Card imgsrc="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTi-8UGYwmCK3hg0wwFzdwXBkqEwdkJSwFq-Q&usqp=CAU"
+                card_category="A Netflix Original Series"
+                card_title="Peaky Blinders "
+                href="https://www.netflix.com/in/title/80002479" /> */}
+        </div>
+    </>
 
 );
 
